878ac57aa6d5fcd3baef8241021642d6c72c5dfa
Adi <aaavs118@gmail.com>
2019-08-29 22:29:12 -0400

    Commit 1

---
+++ app/src/main/java/com/example/retrofitAPI/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.retrofitAPI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bt_add_user.setOnClickListener{
+            Log.i("TEst","TEst")
+        }
+    }
+}
